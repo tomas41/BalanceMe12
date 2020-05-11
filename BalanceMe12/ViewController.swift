a4fff15dee3cab1a8b00056e0c1c17029f67105f
Tomas Kupka <tomas@forzasys.com>
2020-05-11 16:15:18 +0200

    Initial Commit

---
+++ BalanceMe12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BalanceMe12
+//
+//  Created by Tomas on 11.05.20.
+//  Copyright © 2020 ForzaSys AS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
